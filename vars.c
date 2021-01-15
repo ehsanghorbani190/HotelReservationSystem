60e835a77b2536d4f9a3db877b86e2493ca2ca3a
Ehsan Ghorbani <ehsanghorbani190@gmail.com>
2021-01-15 12:52:54 +0330

    Moved arrays to a seprate file for better access over project

---
+++ vars.c
@@ -0,0 +1,7 @@
+struct USER {
+    char username[255] , password[255] , sex[10] , phoneNumber[15],date[11];
+    int age, roomNumber;
+} users[1000] ;
+struct RESERVER {
+    char username[255] , password[255];
+} reversers[6];
