245ffd38335cf23cdf0b1f3777d980316f086b69
Ehsan Ghorbani <ehsanghorbani190@gmail.com>
2021-01-15 15:37:36 +0330

    Commented hardcode in case of admin file gets deleted in vars.c / added some files for data

--- vars.c
+++ vars.c
@@ -1,3 +1,5 @@
+/**#include <stdio.h>
+#include <string.h>*/
 struct USER {
     char username[255] , password[255] , sex[10] , phoneNumber[15],date[11];
     int age, roomNumber;
@@ -8,3 +10,12 @@ struct RESERVER {
 struct ADMIN{
     char username[255] , password[255];
 } admin;
+/**
+int main(){
+    FILE * file = fopen("admin.dat" , "wb");
+    strcpy(admin.username , "admin");
+    strcpy(admin.password , "admin");
+    fwrite(&admin ,sizeof(struct ADMIN) , 1 , file);
+    fclose (file);
+}
+*/
