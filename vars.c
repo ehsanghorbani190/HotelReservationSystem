66b3ee60b3a849f555c25d1588b065320ad5c0e1
Ehsan Ghorbani <ehsanghorbani190@gmail.com>
2021-01-16 20:03:25 +0330

    Moved initialize and summrize funcs to vars.c

--- vars.c
+++ vars.c
@@ -13,6 +13,50 @@ struct RESERVER {
 struct ADMIN{
     char username[255] , password[255];
 } admin;
+void __intialize()
+{
+    FILE *adminfile;
+    adminfile = fopen("admin.dat", "rb");
+    if (!adminfile)
+    {
+        printf("No Admin data found.\n");
+        exit(0);
+    }
+    fread(&admin, sizeof(struct ADMIN), 1, adminfile);
+    fclose(adminfile);
+    FILE *reservefile, *usersfile;
+    reservefile = fopen("reserver.dat", "rb");
+    if (!reservefile)
+        for (int i = 0; i < 6; i++)
+            reservers[i].username[0] = '\0';
+    else
+    {
+        fread(&reservers, sizeof(struct RESERVER), 6, reservefile);
+    }
+    fclose(reservefile);
+    usersfile = fopen("user.dat", "rb");
+    if (!usersfile)
+        for (int i = 0; i < 1000; i++)
+            users[i].roomNumber = -1;
+    else
+    {
+        fread(&users, sizeof(struct USER), 1000, usersfile);
+    }
+    fclose(usersfile);
+}
+void __summrize()
+{
+    FILE *adminfile, *userfile, *reserverfile;
+    adminfile = fopen("admin.dat", "wb");
+    fwrite(&admin, sizeof(struct ADMIN), 1, adminfile);
+    fclose(adminfile);
+    userfile = fopen("user.dat", "wb");
+    fwrite(&users, sizeof(struct USER), 1000, userfile);
+    fclose(userfile);
+    reserverfile = fopen("reserver.dat", "wb");
+    fwrite(&reservers, sizeof(struct RESERVER), 6, reserverfile);
+    fclose(reserverfile);
+}
 /*
 int main(){
     FILE * file = fopen("admin.dat" , "wb");
